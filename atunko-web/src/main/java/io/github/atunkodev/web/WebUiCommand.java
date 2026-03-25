7a3d665b6eecb47a74f23af7c4d4b8657365b412
Jimisola Laursen <jimisola@jimisola.com>
2026-03-25 22:34:01 +0100

    feat(web): auto-open browser when Web UI starts
    Use com.vaadin.open.Open (already a transitive dependency) to
    open the user's default browser after the Jetty server starts.
    
    Signed-off-by: Jimisola Laursen <jimisola@jimisola.com>

--- atunko-web/src/main/java/io/github/atunkodev/web/WebUiCommand.java
+++ atunko-web/src/main/java/io/github/atunkodev/web/WebUiCommand.java
@@ -1,8 +1,11 @@
 package io.github.atunkodev.web;
 
 import com.github.mvysny.vaadinboot.VaadinBoot;
+import com.github.mvysny.vaadinboot.common.WebServer;
+import com.vaadin.open.Open;
 import io.github.atunkodev.core.recipe.RecipeDiscoveryService;
 import io.github.reqstool.annotations.Requirements;
+import java.io.IOException;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -27,7 +30,13 @@ public class WebUiCommand implements Runnable {
     public void run() {
         RecipeHolder.init(discoveryService.discoverAll());
         try {
-            new VaadinBoot().withPort(port).run();
+            new VaadinBoot() {
+                @Override
+                protected void onStarted(WebServer server) throws IOException {
+                    super.onStarted(server);
+                    Open.open("http://localhost:" + port);
+                }
+            }.withPort(port).run();
         } catch (Exception e) {
             throw new RuntimeException("Failed to start Web UI", e);
         }
